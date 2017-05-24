e9317890d39f1d5da50f85cd8d1b9700af6d1fa6
Navid Shokouhi <navid.shokouhi@utdallas.edu>
2017-05-24 11:24:45 +1000

    added audio_tools for input/output functions.

--- unsupervised_vad.py
+++ unsupervised_vad.py
@@ -4,6 +4,8 @@
 # use the vad_help() function for instructions.
 # Navid Shokouhi December 2012.
 
+# Updated: May 2017 for Speaker Recognition collaboration.
+
 # The revised version includes addition features Voice Activity Detection. Including:
 # SFM (Spectral Flatness Measure)
 import wave 
@@ -11,7 +13,6 @@ import struct
 import time 
 import math
 import sys
-#sys.path.append('/usr/share/pyshared/scipy')
 import scipy as sc
 from scipy import cluster
 import numpy as np
@@ -27,67 +28,36 @@ def vad_help():
 	"""
 	print 10*'-','Voice Activity Detection (VAD) tool',10*'-'
 	print 3*' ','Usage:'
-	print 3*' ','python voice_act_dect.py <OPTIONS> <INPUT> <OUTPUT>'+'\n'
+ 	print 3*' ','python unsupervised_vad.py <OPTIONS> <INPUT> <OUTPUT>'+'\n'
 	print 3*' ','OPTIONS:'
-	print 3*' ','-e              only use log-energy for vad (default: Off)'+'\n'
-	print 3*' ','-ez             use zero-crossing-rate and log-energy (default: Off)'+'\n'
-	print 3*' ','-es             use spectral flatness measure and log-energy (default: Off)'+'\n'
-	print 3*' ','-ezs            use zero-crossing-rate, spectral flatness measure and log-energy (default: On)'+'\n'
-	print 3*' ','-l              set output to be labels for each file (0: nonspeech, 1: voiced). Default: On'+'\n'
-	print 3*' ','-i              set output to be indexes for voiced frames. Default: Off'+'\n'
+	print 3*' ','-e     only use log-energy for vad (default: Off)'+'\n'
+    print 3*' ','-ez    use zero-crossing-rate and log-energy (default: Off)'+'\n'
+	print 3*' ','-es    use spectral flatness measure and log-energy (default: Off)'+'\n'
+	print 3*' ','-ezs   use zero-crossing-rate, spectral flatness measure and log-energy (default: On)'+'\n'
+	print 3*' ','-l     set output to be labels for each file (0: nonspeech, 1: voiced). Default: On'+'\n'
+	print 3*' ','-i     set output to be indexes for voiced frames. Default: Off'+'\n'
 
-def read_wav(filename):
-	"""read file in wav format and return output in list format. One value per sample'
-	Input: (filename) string
-		filename	string(including location). Note: '~' is not interpreted as $HOME
-	Output: (s, fs) tuple
-		s			list of samples. Each sample is of type float and s is ranged in [-1,1] 
-		fs			sampling frequency(aka frame rate, python terminology)"""
-	wid=wave.open(filename,'r')
-	if not (wid.getnchannels() == 1):
-		print "Function only accepts single-channel wav files."
-		exit 
-	fs=wid.getframerate() # framerate is the sampling rate
-	n_samples=wid.getnframes() # samples is the same as frames
-	s_raw=wid.readframes(n_samples)
-	s1=struct.unpack_from("%dh"%n_samples,s_raw)
-	# normalize samples to [-1,1]
-	s_max=max(abs(min(s1)),abs(max(s1)))
-	s=[]
-	for i in s1:
-		s.append(i*1.0/s_max)
-	return list(s), fs
+#### Energy tools
+def zero_mean(xframes):
+    """
+        remove mean of framed signal
+        return zero-mean frames.
+        """
+    m = np.mean(xframes,axis=1)
+    xframes = xframes - np.tile(m,(xframes.shape[1],1)).T
+    return xframes
 
-def enframe(s_list, win, inc):
-	"""enframe: Break input list into frames of length win. The frames
-	are overlapped by the amount of win-inc.
-	The name was inspired by the enframe MATLAB function provided in voicebox.
-	Inputs: (s_list, win, inc)
-		s_list: 	input list
-		win: 		frame length
-		inc: 		increment of next frame with respect to the start point of the previous frame
-	Output:
-		frames: 	list of lists. The inner lists are the frames
-	"""
-	win1=int(win)
-	inc1=int(inc)
-	s_temp=s_list[:]#prevent changing the original list
-	n_samples=len(s_temp)
-	n_frames=n_samples/inc1
-	#zeropad in case of mismatch (i.e. ((n_frames*inc1+win1)-n_samples)~=0).
-	for z_ind in range(((n_frames*inc1+win1)-n_samples)):
-		s_temp.append(0.0)
-	frames=[[]]*n_frames
-	for i in range(n_frames):
-		frames[i]=s_temp[i*inc1:i*inc1+win1]
-	return frames
+def frame_power(xframes):
+    return np.mean(np.diagonal(np.dot(xframes,xframes.T)))
 
 def geomean(val_array):
-	"""Geometric mean function to be used in computing the Spectral Flatness measure of a frame (GM/AM)"""
+	"""Geometric mean function to be used in computing the 
+        Spectral Flatness measure of a frame (GM/AM)"""
 	p=0
 	for i in val_array:
 		p+=np.log(i)
 	return math.exp((1.0/len(val_array))*p)
+
 
 def main_vad(frames,e=1,z=1,sf=1):
 	"""main_vad: label frames as voiced(1) and unvoiced(0) and store 
@@ -103,13 +73,7 @@ def main_vad(frames,e=1,z=1,sf=1):
 		vad: 		list of 0's and 1's and same length of frames. 1 indicates frame is voiced.
 					Elements are of type int.
 	"""
-	#Compute log energy of frames
-	edB=[]
-	for i in frames:
-		temp=i[:]
-		e_tmp=math.fsum(np.array(temp)**2)
-		edB.append(10*math.log10(e_tmp))
-		
+			
 	#Alternative method
 	###EPSILON=10**(-12)	
 	###for i in f:
